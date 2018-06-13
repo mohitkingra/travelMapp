0f18698d9923ee4db43ba1b6d09539e6dae3fdfa
Mohit Kingra <mohitkingra@Mohits-MacBook-Pro.local>
2018-06-14 01:38:59 +0800

    Add react-native-maps

--- App.js
+++ App.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { Button, Text, View } from 'react-native';
 import { createStackNavigator } from 'react-navigation'; 
-
-import Map from 'ol/map';
-import { View as vieW } from 'ol/view';
-import Tile from 'ol/layer/tile';
-import TileJSON from 'ol/source/tilejson';
+import MapView from 'react-native-maps';
 
 class HomeScreen extends React.Component {
   render() {
@@ -24,59 +20,34 @@ class HomeScreen extends React.Component {
 class DetailsScreen extends React.Component {
   render() {
 
-      var raster = new Tile({
-        source: new TileJSON({
-          url: 'https://api.tiles.mapbox.com/v3/mapbox.world-dark.json?secure'
-        })
-      });
-/*
-      var style = new ol.style.Style({
-        fill: new ol.style.Fill({
-          color: 'rgba(255, 255, 255, 0.6)'
-        }),
-        stroke: new ol.style.Stroke({
-          color: '#319FD3',
-          width: 1
-        })
-      });
+    getInitialState() {
+      return {
+        region: {
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+         },
+     };
+    }
 
-      var vector = new ol.layer.Vector({
-        source: new ol.source.Vector({
-          url: 'https://openlayers.org/en/v4.6.5/examples/data/topojson/world-110m.json',
-          format: new ol.format.TopoJSON({
-            // don't want to render the full world polygon (stored as 'land' layer),
-            // which repeats all countries
-            layers: ['countries']
-          }),
-          overlaps: false
-        }),
-        style: style
-      });
-*/
-      var map = new Map({
-        layers: [raster],
-        target: 'map',
-        view: new vieW({
-          center: [0, 0],
-          zoom: 1
-        })
-      });
+    onRegionChange(region) {
+     this.setState({ region });
+    }
 
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Travel Map</Text>
-        <Div
-          id="map"
-        />
-        <Button
-          title="Go to Home"
-          onPress={() => this.props.navigation.navigate('Home')}
-        />
-        <Button
-          title="Share"
-          onPress={() => this.props.navigation.navigate('Home')}
-        />
-      </View>
+      <MapView
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+      <MapView
+      region={this.state.region}
+      onRegionChange={this.onRegionChange}
+    />
     );
   }
 }
