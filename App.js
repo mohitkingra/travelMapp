251cfde331417ea073afd4a1c60845160cc44bb3
mohitkingra <mohit.kingra@ekt-digital.com>
2018-06-15 18:10:56 +0800

    Render d3/SVG data

--- App.js
+++ App.js
@@ -1,9 +1,28 @@
 import React from 'react';
-import { Button, Text, View } from 'react-native';
+import { Button, Text, View, StyleSheet, TouchableHighlight } from 'react-native';
 import { createStackNavigator } from 'react-navigation'; 
 import { geoMercator, geoPath } from "d3-geo";
 import { feature } from "topojson-client";
-import Svg, {G} from 'react-native-svg';
+import Svg, {G, Path} from 'react-native-svg';
+
+import worlddata from './world-110m.json'; 
+//import countrydata from './ISO-3166.csv';
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   alignItems: 'center',
+   justifyContent: 'center',
+  },
+  button: {
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 10,
+  },
+});
+
+var selectedcountryName;
+var selectedcountryId;
 
 class WorldMap extends React.Component {
    constructor() {
@@ -15,23 +34,37 @@ class WorldMap extends React.Component {
   
     projection() {
       return geoMercator()
-        .scale(100)
+        .scale(60)
         .translate([800/2, 450/2])
     }
   
     componentDidMount() {
-      fetch("http://localhost:3000/world-110m.json")
-        .then(response => {
-          if (response.status !== 200) {
-            console.log(`There was a problem: ${response.status}`)
-            return
-          }
-          response.json().then(worlddata => {
-            this.setState({
-              worlddata: feature(worlddata, worlddata.objects.countries).features,
-            })
-          })
-        })
+
+      this.setState({
+        worlddata: feature(worlddata, worlddata.objects.countries).features,
+      })
+
+      //countrydata.forEach(function(country){
+        //if(country.name === selectedcountryName){
+        //  selectedcountryId = country.country-code;
+        //}
+
+      //});
+
+      selectedcountryId = 356;
+
+      worlddata.objects.countries.geometries.forEach(function(geometry){
+        console.log("geometry before-----"+ geometry.id);
+      });
+
+      var temp= [];      
+      temp =  worlddata.objects.countries.geometries.filter(geometry => geometry.id === selectedcountryId);
+
+      temp.forEach(function(geometry){
+        console.log("geometry after-----"+ geometry.id);
+      });
+
+
     }
 
   render() {
@@ -40,11 +73,11 @@ class WorldMap extends React.Component {
           <G className="countries">
             {
               this.state.worlddata.map((d,i) => (
-                <path
+                <Path
                   key={ `path-${ i }` }
                   d={ geoPath().projection(this.projection())(d) }
                   className="country"
-                  fill={ `rgba(38,50,56,${ 1 / this.state.worlddata.length * i})` }
+                  fill={ `rgba(0,0,0,${1})` }
                 />
               ))
             }
@@ -55,15 +88,46 @@ class WorldMap extends React.Component {
 }
 
 class HomeScreen extends React.Component {
+  constructor() {
+    
+    super();
+    
+    this.state = { 
+      color: 'white'
+    }
+  }
+  
+  onButtonPress = () => {
+
+    if(this.state.color === 'white') {
+      this.setState({
+        color: 'gray'
+      })
+
+      selectedcountry = "India";
+
+    }
+    else {
+      this.setState({
+        color: 'white'
+      })
+    }
+  }
+
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
+      <View style={styles.container}>
+        <Text>Select Countries</Text>
+        <View style={[styles.button, {backgroundColor:this.state.color}]}>
+          <Button
+            title="India"
+            onPress={this.onButtonPress}
+          />
+        </View>
         <Button
-          title="Go to Details"
-          onPress={() => this.props.navigation.navigate('Details')}
+            title="Show Travel Map"
+            onPress={() => this.props.navigation.navigate('Details')}
         />
-        <WorldMap />
       </View>
     );
   }
@@ -73,10 +137,11 @@ class DetailsScreen extends React.Component {
 
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
+      <View style={styles.container}>
+        <Text>Your Travel Map</Text>
+        <WorldMap />
         <Button
-          title="Go to Home"
+          title="Back"
           onPress={() => this.props.navigation.navigate('Home')}
         />
        </View>
