e2689ddf87d1d148c0c7de78d59203ad0c98c6f1
mohitkingra <mohit.kingra@ekt-digital.com>
2018-06-29 16:48:33 +0800

    Add nested list view

--- src/components/continent.js
+++ src/components/continent.js
@@ -5,35 +5,58 @@ import PropTypes from 'prop-types'
 
 import Item from './item.js';
 
+import NestedListview, { NestedRow } from 'react-native-nested-listview';
+
+const data = [{title: 'Node 1', items: [{title: 'Node 1.1'}, {title: 'Node 1.2'}]}]
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   alignItems: 'center',
+   justifyContent: 'center',
+  },
+  button: {
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 10,
+  },
+});
+
 export default class ContinentList extends React.Component {
   constructor(props) {  
       super(props);
   }
 
   render(){    
-      var continentList = this.props.continents;
+      //var continentList = this.props.continents;
 
-      continentList = continentList.filter(continent => continent.select ===1);
+      //continentList = continentList.filter(continent => continent.select ===1);
 
-      if(continentList.length === 0)
-        continentList = this.props.continents;
+      //if(continentList.length === 0)
+      //  continentList = this.props.continents;
 
       return(
-        continentList.map((continent, index) => {     
-          return(
-            <Item key={"Continent" + index} id={"Continent" + index} name={continent.name} toggleContinent={this.props.toggleContinent} />
-          );
-        })
+        <View style={styles.container}>
+          <NestedListview
+            data={data}
+            getChildrenName={(node) => 'items'}
+            renderNode={(node, level) => (
+            <NestedRow level={level}  /*style={styles.button}*/>
+              <Text>{node.title}</Text>
+            </NestedRow>
+            )}
+          />
+        </View>
       );
-  }
+    }
 }
 
-ContinentList.propTypes = {
-  continents: PropTypes.arrayOf(
-    PropTypes.shape({
-      select: PropTypes.Number,
-      name: PropTypes.string
-    })
-  ),
-  toggleContinent: PropTypes.func
-};
+//ContinentList.propTypes = {
+//  continents: PropTypes.arrayOf(
+//    PropTypes.shape({
+//      select: PropTypes.Number,
+//      name: PropTypes.string
+//    })
+//  ),
+//  toggleContinent: PropTypes.func
+//};
